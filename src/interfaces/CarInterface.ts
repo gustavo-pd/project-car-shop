b96e97840e63aae820de25f3d758e01b1bad835d
GustavoPD <gudalmoro@gmail.com>
2022-07-13 17:28:33 -0300

    req 1 a 3, interfaces model, vehicle e car

---
+++ src/interfaces/CarInterface.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+import { Vehicle } from './VehicleInterface';
+
+export const CarSchema = z.object({
+  doorsQty: z.number({
+    required_error: 'DoorsQty is required',
+    invalid_type_error: 'DoorsQty must be a number',
+  }).gte(2).lte(4),
+
+  seatsQty: z.number({
+    required_error: 'SeatsQty is required',
+    invalid_type_error: 'SeatsQty must be a number',
+  }).gte(2).lte(7),
+});
+
+export type Car = z.infer<typeof CarSchema> & Vehicle;
